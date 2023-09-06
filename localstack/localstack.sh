a5356402c0e0e0aab27cb15b675685397a0885cf
odevpedro <pedrosschmidt2@gmail.com>
2023-09-05 22:55:11 -0300

    aplicação se conectando com o spring boot

---
+++ localstack/localstack.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+aws --endpoint http://localhost:4566 --profile localstack ssm put-parameter --name "/config/spring-boot-localstack_localstack/helloWorld" --value "Hello World Parameter Store" --type String
---
+++ src/test/java/br/com/odevpedro/springbootlocalstack/SpringBootLocalstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.odevpedro.springbootlocalstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLocalstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
