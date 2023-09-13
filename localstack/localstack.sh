85998f9ca10ca073269a5b5a9f442293230337f8
odevpedro <pedrosschmidt2@gmail.com>
2023-09-13 10:23:39 -0300

    adicionando s3

--- localstack/localstack.sh
+++ localstack/localstack.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 aws --endpoint http://localhost:4566 --profile localstack ssm put-parameter --name "/config/spring-boot-localstack_localstack/helloWorld" --value "Hello World Parameter Store" --type String
+
+aws --endpoint http://localhost:4566 --profile localstack s3 mb s3://helloworld
